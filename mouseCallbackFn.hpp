e6216e9bb020ac4dd7622754e9370707790f56f3
Dinesh Veluswamy <79753351+thatcatfromspace@users.noreply.github.com>
2024-12-04 16:25:58 +0530

    Update mouseCallbackFn.hpp

--- mouseCallbackFn.hpp
+++ mouseCallbackFn.hpp
@@ -1,3 +1,16 @@
 void mouse(int button, int state, int x, int y){
-  
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
+        arr[i].x = x;
+        arr[i++].y = SCREEN_HEIGHT - y; // subtracted because height is inverted between mouse and screen
+        std::cout << "mouse @ " << x << '\t' << y << '\n';
+        glBegin(GL_POINTS);
+        glVertex2f(x, SCREEN_HEIGHT - y);
+        glEnd();
+        glFlush();
+    }
+    else if (button == GLUT_RIGHT_BUTTON){
+        glutPostRedisplay(); // cannot call display() as is b/c it is a callback
+    }
 }
+
+// call glutMouseFunc(mouse) after init()
